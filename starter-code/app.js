d976708c00cd927b0c5108e9018925d9a7858429
MELFI PEREZ <perezmelfi@outlook.com>
2019-06-07 16:01:42 -0700

    Completed problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -63,10 +63,24 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+  console.log('Sum of elements: ' + summation);
+  console.log('Product of elements: ' + multi);
+  console.log('String including summed values: ' + thirdElement);
+  console.log('Another string including multiplied values: ' + fourthElement);
+
+
+
+  var summation = sum(sum(a,b)[0],c)[0];
+  var multi = multiply(multiply(a,b)[0], c)[0];
+
+  var thirdElement =  a + ' and ' + b + ' and ' + c + ' sum to ' + summation + '.';
+  var fourthElement = 'The product of ' + a + ' and ' + b +  ' and ' + c + ' is ' + multi + '.';
+
+  return[summation, multi, thirdElement, fourthElement ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
